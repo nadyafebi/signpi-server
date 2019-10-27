20fce4f731a5062df6b04fe4c26056f844a64132
Nadya Djojosantoso <hello@nadyafebi.com>
2019-10-26 23:22:15 -0700

    Add ASL classifiers and change main.py to suit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ graph = tf.get_default_graph()
 set_session(sess)
 
 # Setup model
-model_path = os.path.join(base_dir, 'mnist.h5')
+model_path = os.path.join(base_dir, 'asl_classifier_v1.h5')
 model = load_model(model_path)
 
 # Setup Flask
@@ -43,24 +43,45 @@ def send():
         # Save image to server storage
         files = request.files.getlist('images')
         imgs = []
+        outputs = []
         for file in files:
             save_path = os.path.join(save_dir, file.filename)
             file.save(save_path)
 
             # Read and resize image
-            img = cv2.imread(save_path, cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img, (28, 28))
-            imgs.append(img)
+            img = cv2.imread(save_path, cv2.IMREAD_COLOR)
+            y, x, z = img.shape
+            smol = min(x, y)
+            cropx, cropy = smol, smol
+            startx = x // 2 - cropx // 2
+            starty = y // 2 - cropy // 2
+            img = img[starty:starty + cropy, startx:startx + cropx]
 
-        imgs = np.resize(imgs, (len(imgs), 28, 28, 1))
+            img = cv2.resize(img, (100, 100))
+            cv2.imwrite('x' + file.filename, img)
+            # imgs.append(img)
+            img = np.resize(img, (1, 100, 100, 3))
+            output = model.predict(img)
+            outputs.append(output)
+
+        # imgs = np.resize(imgs, (len(imgs), 100, 100, 3))
 
         # Predict and return result
-        outputs = model.predict(imgs)
+        # outputs = model.predict(imgs)
+        # s = []
+        # m = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+        # for output in outputs:
+        #     s.append(m[np.argmax(output)])
+        # return json.dumps(' '.join(s), cls=NumpyEncoder)
+
+        # Predict and return result
+        # outputs = model.predict(imgs)
         s = []
-        m = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
         for output in outputs:
-            s.append(m[np.argmax(output)])
-        return json.dumps(' '.join(s), cls=NumpyEncoder)
+            x = int(np.argmax(output))
+            s.append(chr(x + 65))
+        print(''.join(s))
+        return json.dumps(''.join(s), cls=NumpyEncoder)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
