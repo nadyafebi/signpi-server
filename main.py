8dcf79e903f98bdcb17137d0ed1ce59bc14d0fb9
Nadya Djojosantoso <hello@nadyafebi.com>
2019-10-26 23:34:52 -0700

    Reorganize main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 import json
 from flask import Flask, request
+import numpy as np
 import tensorflow as tf
 from keras.backend import set_session
 from keras.models import load_model
-import numpy as np
 import cv2
 
+# Helper class to convert Numpy array to JSON
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+# Make image aspect ratio 1:1
+def squarify(img):
+    y, x, c = img.shape
+    smol = min(x, y)
+    start_x = x // 2 - smol // 2
+    start_y = y // 2 - smol // 2
+    return img[start_y:start_y + smol, start_x: start_x + smol]
 
 # Setup folder
 base_dir = os.path.dirname(__file__)
@@ -40,47 +49,33 @@ def send():
     with graph.as_default():
         set_session(sess)
 
-        # Save image to server storage
         files = request.files.getlist('images')
         imgs = []
         outputs = []
         for file in files:
+            # Save image to server storage
             save_path = os.path.join(save_dir, file.filename)
             file.save(save_path)
 
             # Read and resize image
             img = cv2.imread(save_path, cv2.IMREAD_COLOR)
-            y, x, z = img.shape
-            smol = min(x, y)
-            cropx, cropy = smol, smol
-            startx = x // 2 - cropx // 2
-            starty = y // 2 - cropy // 2
-            img = img[starty:starty + cropy, startx:startx + cropx]
-
+            img = squarify(img)
             img = cv2.resize(img, (100, 100))
             cv2.imwrite('x' + file.filename, img)
-            # imgs.append(img)
             img = np.resize(img, (1, 100, 100, 3))
+
+            # Predict image and add to output
             output = model.predict(img)
             outputs.append(output)
 
-        # imgs = np.resize(imgs, (len(imgs), 100, 100, 3))
-
-        # Predict and return result
-        # outputs = model.predict(imgs)
-        # s = []
-        # m = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
-        # for output in outputs:
-        #     s.append(m[np.argmax(output)])
-        # return json.dumps(' '.join(s), cls=NumpyEncoder)
-
-        # Predict and return result
-        # outputs = model.predict(imgs)
+        # Convert output to array of strings
         s = []
         for output in outputs:
             x = int(np.argmax(output))
-            s.append(chr(x + 65))
+            s.append(chr(x + 65)) # 65 = A
         print(''.join(s))
+
+        # Return the JSON
         return json.dumps(''.join(s), cls=NumpyEncoder)
 
 if __name__ == '__main__':
